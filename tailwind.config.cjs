715ed9313e8993c2a758a3e6e12a6b0bf0974839
brian <mr.brianhobo@gmail.com>
2023-07-26 15:13:32 -0700

    feat: initial wesbite layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        background: 'rgb(var(--color-background))',
+        accent: 'rgb(var(--color-accent))',
+      },
+      fontFamily: {
+        mono: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
+        pixel: ["Mondwest", ...defaultTheme.fontFamily.serif],
+      },
+      listStyleType: {
+        bracket: '"> "',
+      }
+    },
 	},
 	plugins: [],
 }
