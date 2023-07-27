f17900f674bef49ab7d0241c5e9d34370139fb59
brian <mr.brianhobo@gmail.com>
2023-07-27 03:09:39 -0700

    feat: add header navbar component

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,29 @@
+import type React from 'react';
+import { useEffect, useState } from 'react';
+
+const ThemeToggle: React.FC<{}> = () => {
+  const [theme, setTheme] = useState(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
+  const handleClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <div className="m-4">
+      <button onClick={handleClick} className="w-10 h-10 text-3xl font-pixel">
+      {theme === "light" ? "☽" : "☀"}
+      </button>
+    </div>
+  )
+}
+
+export default ThemeToggle;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,15 +4,20 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
 	theme: {
 		extend: {
+      backgroundImage: {
+        dots: 'var(--dots)',
+      },
       colors: {
-        background: 'rgb(var(--color-background))',
+        background: 'rgb(var(--background))',
         accent: 'rgb(var(--color-accent))',
+        muted: 'rgb(var(--color-muted))',
       },
       fontFamily: {
         mono: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
-        pixel: ["Mondwest", ...defaultTheme.fontFamily.serif],
+        pixel: ["Mondwest"],
       },
       listStyleType: {
         bracket: '"> "',
