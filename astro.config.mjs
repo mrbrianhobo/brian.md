bcfcacd838c26c9719453effd19a8529a3a78b09
brian <mr.brianhobo@gmail.com>
2023-08-09 06:03:44 -0700

    feat: add analytics tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,21 @@ import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(), 
+    tailwind(), 
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    })
+  ],
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel({
+    analytics: true
+  })
 });
