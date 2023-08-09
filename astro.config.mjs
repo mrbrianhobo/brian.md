a259bce20180e00a3deaacf01ca68d932e9e7e75
brian <mr.brianhobo@gmail.com>
2023-08-09 05:09:10 -0700

    feat: add ssr for 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "hybrid",
+  adapter: vercel()
 });
