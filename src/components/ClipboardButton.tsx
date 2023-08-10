79ff27899ec1c42bbbe15862dffee5b709347165
brian <mr.brianhobo@gmail.com>
2023-08-10 08:12:28 -0700

    fix: update open graph image + ClipboardButton

--- src/components/ClipboardButton.tsx
+++ src/components/ClipboardButton.tsx
@@ -8,16 +8,16 @@ type Props = {
 const ClipboardButton = ({ text }: Props) => {
   const copyToClipboardAndNotify = async () => {
     await navigator.clipboard.writeText(text);
-    toast.success('COPIED TO CLIPBOARD', {
-      position: 'bottom-center',
+    toast.success("COPIED TO CLIPBOARD", {
+      position: "bottom-center",
       className: "dark:shadow-toast font-code font-normal",
       style: {
-        backgroundColor: 'rgb(var(--toast))',
-        color: 'rgb(var(--body))',
+        backgroundColor: "rgb(var(--toast))",
+        color: "rgb(var(--body))",
       },
       iconTheme: {
-        primary: 'rgb(var(--color-accent))',
-        secondary: 'rgb(255, 255, 255)',
+        primary: "rgb(var(--color-accent))",
+        secondary: "rgb(255, 255, 255)",
       },
     });
   }
