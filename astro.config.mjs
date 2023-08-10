47246dde1c988377b78df128b701c6110866edef
brian <mr.brianhobo@gmail.com>
2023-08-10 08:50:36 -0700

    fix: remove analytics ids

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,12 +11,17 @@ export default defineConfig({
     tailwind(), 
     partytown({
       config: {
-        forward: ["dataLayer.push"],
+        forward: ['dataLayer.push'],
       },
     })
   ],
-  output: "hybrid",
+  output: 'hybrid',
   adapter: vercel({
     analytics: true
-  })
+  }),
+  vite: {
+    define: {
+      'import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
+    },
+  },
 });
