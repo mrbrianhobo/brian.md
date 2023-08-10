f05397870b20255cfec7aef0be81dd5618e55d79
brian <mr.brianhobo@gmail.com>
2023-08-10 09:49:14 -0700

    chore: remove comments

--- src/components/LastCheckin.tsx
+++ src/components/LastCheckin.tsx
@@ -19,21 +19,6 @@ type Location = {
 }
 
 const url = 'https://checkins.api.brian.md';
-// const data = `{"id":"64b2fe6830ff4a08854d822b","timestamp":1689452136,"name":"Minnesota Street Projects Gallery","location":{"address":"1275 Minnesota St","city":"San Francisco","state":"CA","country":"US","latitude":37.75424739872958,"longitude":-122.38954229812218}}`
-// const data = `{
-//   "id": "64cb38e11c34e74e5a836425",
-//   "timestamp": 1691039969,
-//   "name": "Alpine Inn",
-//   "location": {
-//     "address": "3915 Alpine Rd",
-//     "city": "Portola Valley",
-//     "state": "CA",
-//     "country": "United States",
-//     "cc": "US",
-//     "latitude": 37.382279388598356,
-//     "longitude": -122.19388050808233
-//   }
-// }`
 
 const LastCheckin = () => {
   const {isLoading, data, error} = useFetch<Checkin>(url);
